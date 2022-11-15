4e71559c21f282046eab9e5de3479763f6454ae9
jensku <jensku@stud.ntnu.no>
2022-11-15 19:40:11 +0100

    slideshow fix, P3 fix

--- Website/scripts/about.js
+++ Website/scripts/about.js
@@ -3,16 +3,6 @@ slides = document.querySelectorAll(".slides");
 let i = 0
 
 function slideshow(){
-    djsjif()
-    i++
-    if( i == slides.length ){
-        i = 0
-    }
-    setTimeout("slideshow()", 3500);
-}
-slideshow()
-
-function djsjif(){
     lastIndex = slides.length -1;
     if(i == 0){
         slides[i].classList.add("active");
@@ -22,8 +12,13 @@ function djsjif(){
         slides[i].classList.add("active");
         slides[i-1].classList.remove("active");
     }
-        
+    
+    i++
+    if( i == slides.length ){
+        i = 0
+    }
 }
 
-
+slideshow();
+setInterval(slideshow, 3500);
 
